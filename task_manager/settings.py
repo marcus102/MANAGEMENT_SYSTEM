ca8b4d47fe3bdc68bc19404a229ffbccb8bd45df
Marcus <wendpanga1004@gmail.com>
2024-02-17 18:15:55 +0100

    all

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -132,15 +132,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 # settings.py
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static/'),  # Add this line
+    os.path.join(BASE_DIR, 'static'),  # Add this line
 ]
 
 STATICFILES_ROOT = [
-    os.path.join(BASE_DIR, 'staticfiles_build', 'static/'),
+    os.path.join(BASE_DIR, 'staticfiles_build', 'static'),
 ]
 
 # Default primary key field type
